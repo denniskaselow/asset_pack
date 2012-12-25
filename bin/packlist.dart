35a07a56a1af242d7fb8d289d337447ad783f0e8
John McCutchan <john@johnmccutchan.com>
2012-12-25 13:41:13 -0800

    Add pack list
    Signed-off-by: John McCutchan <john@johnmccutchan.com>

---
+++ bin/packlist.dart
@@ -0,0 +1,51 @@
+import 'dart:io';
+import 'dart:json';
+import 'package:asset_pack/asset_pack_file.dart';
+
+AssetPackFile openAssetPackFile(String path) {
+  File out = new File(path);
+  String contents;
+  try {
+    contents = out.readAsStringSync();
+  } catch (_) {
+    print('Could not open existing asset pack file.');
+    print('Creating new assset pack.');
+    // Return empty asset pack file.
+    return new AssetPackFile();
+  }
+  List<Map> json;
+  try {
+    json = JSON.parse(contents);
+  } catch (e) {
+    print(e);
+    print('Could not parse existing asset pack file.');
+    print('Creating new assset pack.');
+    // Return empty asset pack file.
+    return new AssetPackFile();
+  }
+  print('Loaded existing asset pack file.');
+  return new AssetPackFile.fromJson(json);
+}
+
+String inPath = '/Users/johnmccutchan/workspace/assetpack/test/testpack';
+String outPath = '$inPath.pack';
+
+main() {
+  bool verbose = true;
+  AssetPackFile packFile = openAssetPackFile(outPath);
+  List<AssetPackFileAsset> assets = packFile.assets.values;
+  assets.sort((a, b) => Comparable.compare(a.name, b.name));
+  assets.sort((a, b) => Comparable.compare(a.type, b.type));
+  int count = 0;
+  assets.forEach((f) {
+    var name = f.name;
+    var url = f.url;
+    var type = f.type;
+    print('$count $name $type ($url)');
+    if (verbose) {
+      print('$count   L: ${f.loadArguments}');
+      print('$count   I: ${f.importArguments}');
+    }
+    count++;
+  });
+}
