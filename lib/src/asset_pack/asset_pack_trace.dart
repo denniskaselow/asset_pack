a99647201c0819d0b45573d532f2adbaa02dded7
Oliver Sand <oliver.sand@tentaclelabs.com>
2013-12-23 10:57:52 +0100

    #33 Don't use window.performance.now() because it isn't supported in Safari. Use Stopwatch instead

--- lib/src/asset_pack/asset_pack_trace.dart
+++ lib/src/asset_pack/asset_pack_trace.dart
@@ -135,6 +135,7 @@ class AssetPackTraceViewer {
 }
 
 class AssetPackTrace {
+  final _stopwatch = new Stopwatch()..start();
   final _streamCtrl = new StreamController<AssetPackTraceEvent>(sync: true);
 
   Stream<AssetPackTraceEvent> asStream() => _streamCtrl.stream;
@@ -165,7 +166,7 @@ class AssetPackTrace {
 
   void assetEvent(Asset asset, String type, String msg) {
     var label = (msg == null) ? asset.url : "${asset.url} >> ${msg}";
-    var now = (window.performance.now() * 1000).toInt();
+    var now = _stopwatch.elapsedMicroseconds;
     var event = new AssetPackTraceEvent(type, label, now);
     _streamCtrl.add(event);
   }
